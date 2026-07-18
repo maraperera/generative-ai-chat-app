27aed5efc01cc74b4a6b46c1faa8748546d9865c
maraperera <reach.anthony.perera.1@gmail.com>
2026-07-18 17:55:09 +1000

    Add Get a response

--- labfiles/foundry-chat/python/chat-app/chat-app.py
+++ labfiles/foundry-chat/python/chat-app/chat-app.py
@@ -39,6 +39,20 @@ def main():
                 continue
 
             # Get a response
+            completion = openai_client.chat.completions.create(
+                model=model_deployment,
+                messages=[
+                    {
+                        "role": "system",
+                        "content": "You are a helpful AI assistant that answers questions and provides information."
+                    },
+                    {
+                        "role": "user",
+                        "content": input_text
+                    }
+                ]
+            )
+            print(completion.choices[0].message.content)
             
 
     except Exception as ex:
